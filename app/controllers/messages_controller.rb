65d924032a8e76cdf576524cdde61b064d0da015
is044088 <is044088@gmail.com>
2019-08-24 15:07:58 +0900

    incremental_search_ver2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,8 @@
 class MessagesController < ApplicationController
   before_action :set_group
   def index
-    
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    # @group = Group.find(params[:id])
   end
 
   def create
